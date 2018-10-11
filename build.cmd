50281b95da2e8bb18c1aee61410afc165555272c
Nick Guerrera <nicholg@microsoft.com>
2018-10-11 11:27:00 -0700

    Fix bug in build.cmd

--- build.cmd
+++ build.cmd
@@ -13,12 +13,13 @@ if not defined WPF_REPO (
 msbuild /m /v:m %WPF_REPO%\src\WPF\src\PresentationBuildTasks || goto :eof
 
 :: build Microsoft.NET.Sdk.Wpf
-msbuild /m /v:m %WPF_REPO%\pkg\microsoft.net.sdk.wpf && msbuild /m /v:m  goto :eof
+msbuild /m /v:m %WPF_REPO%\pkg\microsoft.net.sdk.wpf || goto :eof
 
 :: clear package cache
 if exist %~dp0.packages ( 
   rd /s /q %~dp0.packages || goto :eof
 )
+
 
 :: build sample rpoject
 call %~dp0run.cmd dotnet build %~dp0WPFSample.Core /bl
